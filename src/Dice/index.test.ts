e39f8a6e7dfdbd073c1f15276a31eaca0f52c34b
Damien <damien@bodhi.digital>
2022-12-18 10:38:15 -0600

    more config and adds test

--- src/Dice/index.test.ts
+++ src/Dice/index.test.ts
@@ -1,9 +1,75 @@
-import { Dice } from "./index";
+import Dice from "./index";
 
+const dice = new Dice();
 describe("Dice", () => {
-  it("should log 'Hello World'", () => {
-    const spy = jest.spyOn(console, "log");
-    new Dice();
-    expect(spy).toHaveBeenCalledWith("Hello World");
+  describe("Additional Methods", () => {
+    // check that the private method "sortedBy" is working
+    it("should sort the results highest to lowest", () => {
+      const d = new Dice();
+      const result = d.D6(6).drop("low", 3).roll();
+      expect(result.length).toBe(3);
+      // first check that the 1st restult is higher than the 2nd
+      expect(result[0]).toBeGreaterThanOrEqual(result[1]!);
+      // then check that the 2nd result is higher than the 3rd
+      expect(result[1]).toBeGreaterThanOrEqual(result[2]!);
+    });
+    it("should drop the highest rolls", () => {
+      const result = dice.D6(3).drop("high", 1).roll();
+      expect(result.length).toBe(2);
+    });
+  });
+  describe("D20", () => {
+    it("should roll a D20", () => {
+      const result = dice.D20().roll();
+      expect(result.length).toBe(1);
+      expect(typeof result[0]).toBe("number");
+      expect(result[0]).toBeGreaterThanOrEqual(1);
+      expect(result[0]).toBeLessThanOrEqual(20);
+    });
+  });
+  describe("D12", () => {
+    it("should roll a D12", () => {
+      const result = dice.D12().roll();
+      expect(result.length).toBe(1);
+      expect(typeof result[0]).toBe("number");
+      expect(result[0]).toBeGreaterThanOrEqual(1);
+      expect(result[0]).toBeLessThanOrEqual(12);
+    });
+  });
+  describe("D10", () => {
+    it("should roll a D10", () => {
+      const result = dice.D10().roll();
+      expect(result.length).toBe(1);
+      expect(typeof result[0]).toBe("number");
+      expect(result[0]).toBeGreaterThanOrEqual(1);
+      expect(result[0]).toBeLessThanOrEqual(10);
+    });
+  });
+  describe("D8", () => {
+    it("should roll a D8", () => {
+      const result = dice.D8().roll();
+      expect(result.length).toBe(1);
+      expect(typeof result[0]).toBe("number");
+      expect(result[0]).toBeGreaterThanOrEqual(1);
+      expect(result[0]).toBeLessThanOrEqual(8);
+    });
+  });
+  describe("D6", () => {
+    it("should roll a D6", () => {
+      const result = dice.D6().roll();
+      expect(result.length).toBe(1);
+      expect(typeof result[0]).toBe("number");
+      expect(result[0]).toBeGreaterThanOrEqual(1);
+      expect(result[0]).toBeLessThanOrEqual(6);
+    });
+  });
+  describe("D4", () => {
+    it("should roll a D4", () => {
+      const result = dice.D4().roll();
+      expect(result.length).toBe(1);
+      expect(typeof result[0]).toBe("number");
+      expect(result[0]).toBeGreaterThanOrEqual(1);
+      expect(result[0]).toBeLessThanOrEqual(4);
+    });
   });
 });
--- src/Dice/index.ts
+++ src/Dice/index.ts
@@ -1,5 +1,98 @@
-export class Dice {
-  constructor() {
-    console.log("Hello World");
+type T_sortOptions = "high" | "low";
+
+function sortedBy(type: T_sortOptions, sorted: number[]) {
+  return sorted.sort((a, b) => (type === "high" ? a - b : b - a));
+}
+
+class Dice {
+  /** Internal State */
+  private results: number[] = [];
+
+  // Internal methods
+  private pushRoll(sides: number, times: number) {
+    for (let i = 0; i < times; i++) {
+      this.results.push(Math.floor(Math.random() * sides) + 1);
+    }
+  }
+
+  // Roll methods
+
+  /**
+   * Roll a D20.
+   * @param x pass X to roll multiple times.
+   */
+  D20(x = 1) {
+    this.pushRoll(20, x);
+    return this;
+  }
+  /**
+   * Roll a D12.
+   * @param x pass X to roll multiple times.
+   */
+  D12(x = 1) {
+    this.pushRoll(12, x);
+    return this;
+  }
+  /**
+   * Roll a D10.
+   * @param x pass X to roll multiple times.
+   */
+  D10(x = 1) {
+    this.pushRoll(10, x);
+    return this;
+  }
+  /**
+   * Roll a D8.
+   * @param x pass X to roll multiple times.
+   */
+  D8(x = 1) {
+    this.pushRoll(8, x);
+    return this;
+  }
+  /**
+   * Roll a D6.
+   * @param x pass X to roll multiple times.
+   */
+  D6(x = 1) {
+    this.pushRoll(6, x);
+    return this;
+  }
+  /**
+   * Roll a D4.
+   * @param x pass X to roll multiple times.
+   */
+  D4(x = 1) {
+    this.pushRoll(4, x);
+    return this;
+  }
+
+  // Ultility methods
+
+  /**
+   * Drop the lowest or highest rolls.
+   * @param type Which type of rolls to drop.
+   * @param n The number of rolls to drop.
+   */
+  drop(type: T_sortOptions, n: number) {
+    const notDropped = sortedBy(type, this.results).slice(
+      0,
+      this.results.length - n
+    );
+    this.results = [...notDropped];
+    return this;
+  }
+
+  // Return methods
+
+  /**
+   * Main Return Method
+   * Roll returns the results of the rolls made and resets the results array.
+   */
+  roll() {
+    const copyResults = [...this.results];
+    this.results = [];
+    return copyResults;
   }
 }
+
+export default Dice;
